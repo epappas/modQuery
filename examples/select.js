4a2f89349d53206f5177a3e9ea6fe69d9eceb9b4
Evangelos Pappas <papas.evagelos@gmail.com>
2013-05-21 17:29:21 +0300

    a select() example

---
+++ examples/select.js
@@ -0,0 +1,24 @@
+var modQ = new require("../commons/modQuery.js");
+
+var modQuery = new modQ({
+	dbArgs: {
+		host              : "localhost",
+		user              : "bartender",
+		password          : "beerIsAwesome",
+		database          : "my_pub"
+	}
+});
+
+modQuery.newModQuery()
+	.select()
+	.from("beers")
+	.filterBy("beers", "id").equals(1)
+	.execute(function(rows, err, sql) {
+		if(err){
+			// if error, show what the heck was executed
+			console.log(sql);
+			console.log(err);
+		}
+		console.log(sql);
+		console.log(rows);
+	});
